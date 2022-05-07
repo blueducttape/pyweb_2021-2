2326e0fdb3a8871692ef2eb0396f22ebe2de7236
uliana <val.uliana@yandex.ru>
2022-05-07 16:57:27 +0300

    add_requirements.txt

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG"))
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")] # с каких IP разрешаются запросы
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]  # с каких IP разрешаются запросы
 
 # Application definition
 
